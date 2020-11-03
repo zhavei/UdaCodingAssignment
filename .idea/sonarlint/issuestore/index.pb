95225ad858fd17a586e8cb0be9e4bb1f5c461b4b
zhavei <64249579+zhavei@users.noreply.github.com>
2020-11-03 13:17:06 +0700

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+L
+src/main/kotlin/udacoding.kt,0\e\0e64f505e463ad5c3da67b5abf37ecf24a91e212
---
+++ src/main/kotlin/functions.kt
@@ -0,0 +1,13 @@
+fun main() {
+var sum = functionReturn(34.5, 78.0)
+    println(sum)
+
+}
+
+fun functionReturn(num1:Double, num2:Double): String {
+    var sum = num1 + num2;
+    return sum.toString()
+}
+
+
+
